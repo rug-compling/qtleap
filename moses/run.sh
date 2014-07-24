95de8126d508207249314333eabe5d812d856d8a
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2014-07-24 14:17:12 +0200

    moses/run.sh: code documentation

--- moses/run.sh
+++ moses/run.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+#
+# This checks if the mosesservers and mserver are running.
+# If not, it starts them.
+# If this is called from crontab, messages will be mailed to owner of crontab file
+#
+# Run this before the first time you call this script:
+#    echo 1 > pid.txt
+#    echo 1 > pid1.txt
+#    echo 1 > pid2.txt
+#    touch mserver.out
+#
+
 if [ "`hostname -s`" != "zardoz" ]
 then
-    echo Dit moet op zardoz
+    echo This must be run on zardoz
     exit 1
 fi
 
 
 cd `dirname $0`
 
-# dit is 10% van het geheugen (swap niet meegerekend)
+# This is 10% of memory (not counting swap)
 ulimit -v 52830340
 
 export LD_LIBRARY_PATH=/net/aps/64/opt/xmlrpc-c/lib
@@ -19,6 +31,7 @@ case "`ps -p $(< pid1.txt)`" in
 	;;
     *)
 	echo Starting mosesserver en-nl
+
 	/net/aps/64/opt/moses/mosesdecoder/bin/mosesserver -f corpus/moses-en-nl.ini --server-port 9071 >/dev/null 2>/dev/null &
 	echo $! > pid1.txt
 	;;
@@ -29,6 +42,7 @@ case "`ps -p $(< pid2.txt)`" in
 	;;
     *)
 	echo Starting mosesserver nl-en
+
 	/net/aps/64/opt/moses/mosesdecoder/bin/mosesserver -f corpus/moses-nl-en.ini --server-port 9072 >/dev/null 2>/dev/null &
 	echo $! > pid2.txt
 esac
@@ -37,8 +51,12 @@ case "`ps -p $(< pid.txt)`" in
     *mserver*)
 	;;
     *)
+	# Errors of previous run
 	cat mserver.out
+
 	echo Starting mserver
+
+	# Don't redirect to mserver.log, that file is used by mserver
 	./mserver > mserver.out 2>&1 &
 	echo $! > pid.txt
 	;;
